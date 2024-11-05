2773b4e5659c695fdecd4cb2baf708816b90c7df
Pproblems95 <l21212001@tectijuana.edu.mx>
2024-11-04 21:01:38 -0800

    modal y funcion para dividir textos en lobby listos

--- app/(tabs)/lobby.jsx
+++ app/(tabs)/lobby.jsx
@@ -1,31 +1,88 @@
-import { Dimensions, Pressable, StyleSheet, Text, TextInput, View } from 'react-native'
-import React from 'react'
+import { Dimensions, Pressable, StyleSheet, Text, TextInput, View, ScrollView, Modal, Button } from 'react-native'
+import React, { useEffect, useState } from 'react'
 import { router } from 'expo-router'
 
 const lobby = () => {
   
+  const [text, SetText] = useState('')
+  const [sendText, SetSendText] = useState([])
+  const [word, SetWord] = useState('')
+  const [sendWord, SetWordSend] = useState('')
+  const [isOpen, SetOpen] = useState(false)
+  const [errorMessage, SetErrorMessage] = useState('')
   const screenWidth = Dimensions.get('screen').width
+  const screenHeight = Dimensions.get('screen').height
+  useEffect(() => {
+    console.log(sendText) 
+  }, [sendText])
+  useEffect(() => {
+    if(errorMessage != '')
+    SetOpen(true)
+  }, [errorMessage])
   return (
-    <View style={{display:'flex', flex:1, padding:5}}>
+    <>
+    <ScrollView style={{display:'flex', padding:5}}>
       <View style={{backgroundColor:'black', borderRadius:20, justifyContent:'space-between', flex:1, display:'flex', marginBottom:10}}> 
-        <TextInput placeholder='Escribe o pega el texto a traducir aquí!' numberOfLines={4} multiline placeholderTextColor='silver' style={{textAlign:'center', color:'white' ,  fontSize:25}}/>
+        <TextInput placeholder='Escribe o pega el texto a traducir aquí!' numberOfLines={4} multiline placeholderTextColor='silver' style={{textAlign:'center', color:'white' ,  fontSize:25}}
+        value={text} onChangeText={(e) => {
+          SetText(e)
+        }}/>
         <Pressable style={{backgroundColor:'white', borderRadius:20, alignSelf:'flex-end', padding:5, margin:5}} onPress={() => {
-          router.navigate("textTranslated")
+          if(text.length > 0){
+            const chunks = text.split('\n')
+            SetSendText(chunks)
+            console.log('h')
+          }
+          else{
+            SetErrorMessage('Por favor ingresa un texto primero')
+            
+          }
+          
         }}>
           <Text style={{fontSize:20}}>Enviar texto</Text>
         </Pressable>
       </View>
-      <View style={{ display:'flex', flex:1, justifyContent:'space-between', backgroundColor:'white', borderRadius:20, marginTop:10  }}>
+      <View style={{ display:'flex',  justifyContent:'space-between', backgroundColor:'white', borderRadius:20, marginTop:10  }}>
         <Text style={{fontSize:30, textAlign:'center'}}>¿Deseas buscar alguna palabra específica?</Text>
-        <TextInput style={{borderBottomColor:'black', borderBottomWidth:1, marginHorizontal:screenWidth*0.2}}/>
+        <TextInput style={{borderBottomColor:'black', borderBottomWidth:1, marginHorizontal:screenWidth*0.2}} value={word} onChangeText={(e) => {
+          SetWord(e)
+        }}/>
         <Pressable style={{alignSelf:'flex-end', padding:10, margin:10, backgroundColor:'black', borderRadius:20, marginBottom:10}} onPress={() => {
-          router.navigate("textTranslated")
+          if(word.length > 0 && !word.includes(' ')){
+            SetSendText(word)
+          }
+          else{
+            SetErrorMessage('Favor de ingresar una única palabra')
+          }
         }}>
         <Text style={{fontSize:20, color:'white'}}>Enviar palabra</Text>
       </Pressable>
       </View>
       
+    </ScrollView>
+    <Modal visible={isOpen} transparent={true} animationType="fade">
+  <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: 'rgba(0, 0, 0, 0.5)' }}>
+    <View style={{ width: screenWidth*0.7,padding: 20, backgroundColor: 'white', borderRadius: 10, borderColor: 'black', borderWidth: 1, display:'flex' }}>
+      <View style={{borderBottomColor:'gray', borderBottomWidth:1, display:'flex', padding:10 }}>
+        <Text style={{ textAlign: 'center', color: 'black', fontSize:30, fontWeight:'bold' }}>AVISO</Text>
+      </View>
+      <View  style={{borderBottomColor:'gray', borderBottomWidth:1, padding:10 }}  >
+        <Text style={{textAlign:'center', fontSize:20}}>{errorMessage}</Text>
+      </View>
+      <View style={{display:'flex', alignSelf:'flex-end', padding:10 }}>
+        <Pressable style={{backgroundColor:'black', borderRadius:20, padding:10}}  onPress={() => {
+          SetErrorMessage('')
+          SetOpen(false)
+        }}>
+          <Text style={{color:'white', fontSize:20}}>Cerrar</Text>
+        </Pressable>
+      </View>
+      
+      
     </View>
+  </View>
+</Modal>
+    </>
   )
 }
 
